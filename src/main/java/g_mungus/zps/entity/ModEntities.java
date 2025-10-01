5ad72bb6fb93f20b80cc682aa20f9c1e691f6440
James Green <jgreenop22@gmail.com>
2025-10-01 16:18:01 -0700

    fix octo controller starlance compat
    (cherry picked from commit c6bdbf6bd6ce4fb3f023e2f11f29b7aa5b1879a6)

--- src/main/java/g_mungus/zps/entity/ModEntities.java
+++ src/main/java/g_mungus/zps/entity/ModEntities.java
@@ -13,6 +13,6 @@ public class ModEntities {
 
     public static final DeferredHolder<EntityType<?>, EntityType<OctoMountingEntity>> OCTO_MOUNTING = ENTITIES.register("octo_mounting_seat",
         () -> EntityType.Builder.<OctoMountingEntity>of(OctoMountingEntity::new, MobCategory.MISC)
-            .sized(0.6f, 0.6f)
+            .sized(0.5f, 0.5f)
             .build("octo_mounting_seat"));
 } 
