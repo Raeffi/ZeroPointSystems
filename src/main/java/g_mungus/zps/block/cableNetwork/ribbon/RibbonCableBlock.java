0dca2b21a72329685ef96816d27c9830728c14c7
James Green <jgreenop22@gmail.com>
2026-03-23 00:09:58 -0700

    Logic seems solid

--- src/main/java/g_mungus/zps/block/cableNetwork/ribbon/RibbonCableBlock.java
+++ src/main/java/g_mungus/zps/block/cableNetwork/ribbon/RibbonCableBlock.java
@@ -5,6 +5,7 @@ import g_mungus.zps.block.cableNetwork.core.CableComponentBlock;
 import g_mungus.zps.block.cableNetwork.core.Channels;
 import g_mungus.zps.block.cableNetwork.core.NetworkNode;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -12,7 +13,7 @@ import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 
-import java.util.List;
+import java.util.*;
 
 public class RibbonCableBlock extends CableComponentBlock {
 
@@ -58,7 +59,76 @@ public class RibbonCableBlock extends CableComponentBlock {
 
     @Override
     public void updateConnections(BlockState state, Level level, BlockPos pos) {
-        //TODO
+        BlockState newState = getNewBlockState(state, level, pos);
+
+        if (!state.equals(newState)) {
+            level.setBlock(pos, newState, 3);
+            updateNetwork(pos, level);
+        }
+    }
+
+    private BlockState getNewBlockState(BlockState state, Level level, BlockPos pos) {
+        RibbonCableOrientation current = state.getValue(ORIENTATION);
+        RibbonCableOrientation newOrientation = current;
+
+        List<Direction> selected = new ArrayList<>();
+
+        List<Direction> available = new ArrayList<>();
+        for (Direction dir : Direction.values()) {
+            if (canConnect(pos, pos.relative(dir), level)) {
+                available.add(dir);
+            }
+        }
+
+        // Keep existing connections if still valid
+        if (available.contains(current.direction_a)) {
+            selected.add(current.direction_a);
+        }
+        if (available.contains(current.direction_b)) {
+            selected.add(current.direction_b);
+        }
+
+        List<Direction> preservedDirections = new ArrayList<>(selected);
+
+        // Add new connections if there is space
+        for (Direction dir : available) {
+            if (!selected.contains(dir) && selected.size() < 2) {
+                selected.add(dir);
+            }
+        }
+
+        // Needs new orientation if we have a connection in a new direction
+        boolean needNewOrientation = selected.stream().anyMatch(it ->
+                it != current.direction_a && it != current.direction_b
+        );
+
+        // Find new orientation, preserving existing twisted-ness
+        if (needNewOrientation) {
+            Map<Direction.Axis, RibbonCableOrientation.TwistState> twistRequirements = new HashMap<>();
+
+            for (var axis: Direction.Axis.values()) {
+                twistRequirements.put(axis, RibbonCableOrientation.TwistState.EITHER);
+            }
+            for (var dir : preservedDirections) {
+                Direction.Axis axis = dir.getAxis();
+                twistRequirements.put(axis, current.getTwistState(axis));
+            }
+
+            for (var orientation : RibbonCableOrientation.values()) {
+                if (new HashSet<>(List.of(orientation.direction_a, orientation.direction_b)).containsAll(selected)
+                        && twistRequirements.entrySet().stream().allMatch(entry ->
+                            entry.getValue().match(orientation.getTwistState(entry.getKey())))
+                ) {
+                    newOrientation = orientation;
+                    break;
+                }
+            }
+        }
+
+        return state
+                .setValue(ORIENTATION, newOrientation)
+                .setValue(CONNECTED_A, available.contains(newOrientation.direction_a))
+                .setValue(CONNECTED_B, available.contains(newOrientation.direction_b));
     }
 
     @Override
--- src/main/java/g_mungus/zps/block/cableNetwork/ribbon/RibbonCableOrientation.java
+++ src/main/java/g_mungus/zps/block/cableNetwork/ribbon/RibbonCableOrientation.java
@@ -8,57 +8,90 @@ import static net.minecraft.core.Direction.*;
 
 public enum RibbonCableOrientation implements StringRepresentable {
 
-    NORTH_SOUTH(NORTH, SOUTH, false, false, false),
-    NORTH_SOUTH_ALT(NORTH, SOUTH, false, false, true),
-    EAST_WEST(EAST, WEST, false, false, false),
-    EAST_WEST_ALT(EAST, WEST, true, false, false),
-    UP_DOWN(UP, DOWN, false, false, false),
-    UP_DOWN_ALT(UP, DOWN, false, true, false),
+    NORTH_SOUTH(NORTH, SOUTH, false),
+    NORTH_SOUTH_ALT(NORTH, SOUTH, true),
+    EAST_WEST(EAST, WEST, false),
+    EAST_WEST_ALT(EAST, WEST, true),
+    UP_DOWN(UP, DOWN, false),
+    UP_DOWN_ALT(UP, DOWN, true),
 
-    UP_NORTH(UP, NORTH, false, false, false),
-    UP_NORTH_ALT(UP, NORTH, false, true, true),
-    UP_EAST(UP, EAST, false, true, false),
-    UP_EAST_ALT(UP, EAST, true, false, false),
-    UP_SOUTH(UP, SOUTH, false, false, false),
-    UP_SOUTH_ALT(UP, SOUTH, false, true, true),
-    UP_WEST(UP, WEST, false, true, false),
-    UP_WEST_ALT(UP, WEST, true, false, false),
+    UP_NORTH(UP, NORTH, false),
+    UP_NORTH_ALT(UP, NORTH, true),
+    UP_EAST(UP, EAST, false),
+    UP_EAST_ALT(UP, EAST, true),
+    UP_SOUTH(UP, SOUTH, false),
+    UP_SOUTH_ALT(UP, SOUTH, true),
+    UP_WEST(UP, WEST, false),
+    UP_WEST_ALT(UP, WEST, true),
 
-    DOWN_NORTH(DOWN, NORTH, false, false, false),
-    DOWN_NORTH_ALT(DOWN, NORTH, false, true, true),
-    DOWN_EAST(DOWN, EAST, false, true, false),
-    DOWN_EAST_ALT(DOWN, EAST, true, false, false),
-    DOWN_SOUTH(DOWN, SOUTH, false, false, false),
-    DOWN_SOUTH_ALT(DOWN, SOUTH, false, true, true),
-    DOWN_WEST(DOWN, WEST, false, true, false),
-    DOWN_WEST_ALT(DOWN, WEST, true, false, false),
+    DOWN_NORTH(DOWN, NORTH, false),
+    DOWN_NORTH_ALT(DOWN, NORTH, true),
+    DOWN_EAST(DOWN, EAST, false),
+    DOWN_EAST_ALT(DOWN, EAST, true),
+    DOWN_SOUTH(DOWN, SOUTH, false),
+    DOWN_SOUTH_ALT(DOWN, SOUTH, true),
+    DOWN_WEST(DOWN, WEST, false),
+    DOWN_WEST_ALT(DOWN, WEST, true),
 
-    NORTH_EAST(NORTH, EAST, false, false, false),
-    NORTH_EAST_ALT(NORTH, EAST, true, false, true),
-    EAST_SOUTH(EAST, SOUTH, false, false, false),
-    EAST_SOUTH_ALT(EAST, SOUTH, true, false, true),
-    SOUTH_WEST(SOUTH, WEST, false, false, false),
-    SOUTH_WEST_ALT(SOUTH, WEST, true, false, true),
-    WEST_NORTH(WEST, NORTH, false, false, false),
-    WEST_NORTH_ALT(WEST, NORTH, true, false, true)
+    NORTH_EAST(NORTH, EAST, false),
+    NORTH_EAST_ALT(NORTH, EAST, true),
+    EAST_SOUTH(EAST, SOUTH, false),
+    EAST_SOUTH_ALT(EAST, SOUTH, true),
+    SOUTH_WEST(SOUTH, WEST, false),
+    SOUTH_WEST_ALT(SOUTH, WEST, true),
+    WEST_NORTH(WEST, NORTH, false),
+    WEST_NORTH_ALT(WEST, NORTH, true)
 ;
 
     public final Direction direction_a;
     public final Direction direction_b;
-    public final boolean twisted_x;
-    public final boolean twisted_y;
-    public final boolean twisted_z;
+    public final boolean twisted_a;
+    public final boolean twisted_b;
 
-    RibbonCableOrientation(Direction directionA, Direction directionB, boolean twistedX, boolean twistedY, boolean twistedZ) {
+    RibbonCableOrientation(Direction directionA, Direction directionB, boolean alt) {
         direction_a = directionA;
         direction_b = directionB;
-        twisted_x = twistedX;
-        twisted_y = twistedY;
-        twisted_z = twistedZ;
+
+        if (directionA.getNormal().getY() != 0 && directionB.getNormal().getY() == 0) {
+            boolean out = alt;
+            if (directionB.getAxis() == Axis.Z) {
+                out = !out;
+            }
+            twisted_a = out;
+        } else {
+            twisted_a = alt;
+        }
+
+        if (directionB.getNormal().getY() != 0 && directionA.getNormal().getY() == 0) {
+            boolean out = alt;
+            if (directionA.getAxis() == Axis.Z) {
+                out = !out;
+            }
+            twisted_b = out;
+        } else {
+            twisted_b = alt;
+        }
     }
 
     @Override
     public @NotNull String getSerializedName() {
         return this.name().toLowerCase();
+    }
+
+    public TwistState getTwistState(Axis axis) {
+        boolean aOnAxis = direction_a.getAxis() == axis;
+        boolean bOnAxis = direction_b.getAxis() == axis;
+        if (aOnAxis) return twisted_a ? TwistState.TWISTED : TwistState.NORMAL;
+        if (bOnAxis) return twisted_b ? TwistState.TWISTED : TwistState.NORMAL;
+        return TwistState.EITHER;
+    }
+
+    public enum TwistState {
+        NORMAL, TWISTED, EITHER;
+
+        public boolean match(TwistState other) {
+            if (this == EITHER || other == EITHER) return true;
+            return this == other;
+        }
     }
 }
